0ef65fcfc099de19ba4affd9e9b64adbc506804b
Maxim <korkinwow@gmail.com>
2022-12-17 01:57:00 +0700

    Add attack tests for bishop

--- tests/Bishop.spec.ts
+++ tests/Bishop.spec.ts
@@ -46,3 +46,34 @@ test('Test bishop moves', () => {
   expect(bishop.isCanMove(1, 4, false)).toBe(false); // Move down by 3
   expect(bishop.isCanMove(4, 7, false)).toBe(false); // Move right by 3
 });
+
+test('Test bishop moves', () => {
+
+  const bishop = new Bishop({
+    row: 4,
+    column: 4,
+    color: ChessmanColor.White,
+  });
+
+  expect(bishop.isCanMove(5, 5, true)).toBe(true); // Attack diagonally by 1
+  expect(bishop.isCanMove(5, 3, true)).toBe(true); // Attack diagonally by 1
+  expect(bishop.isCanMove(3, 5, true)).toBe(true); // Attack diagonally by 1
+  expect(bishop.isCanMove(3, 3, true)).toBe(true); // Attack diagonally by 1
+
+  expect(bishop.isCanMove(7, 7, true)).toBe(true); // Attack diagonally by 3
+  expect(bishop.isCanMove(7, 1, true)).toBe(true); // Attack diagonally by 3
+  expect(bishop.isCanMove(1, 1, true)).toBe(true); // Attack diagonally by 3
+  expect(bishop.isCanMove(1, 7, true)).toBe(true); // Attack diagonally by 3
+
+  expect(bishop.isCanMove(10, 10, true)).toBe(false); // Attack away from board
+
+  expect(bishop.isCanMove(5, 4, true)).toBe(false); // Attack up by 1
+  expect(bishop.isCanMove(4, 3, true)).toBe(false); // Attack left by 1
+  expect(bishop.isCanMove(3, 4, true)).toBe(false); // Attack down by 1
+  expect(bishop.isCanMove(4, 5, true)).toBe(false); // Attack right by 1
+
+  expect(bishop.isCanMove(7, 4, true)).toBe(false); // Attack up by 3
+  expect(bishop.isCanMove(4, 1, true)).toBe(false); // Attack left by 3
+  expect(bishop.isCanMove(1, 4, true)).toBe(false); // Attack down by 3
+  expect(bishop.isCanMove(4, 7, true)).toBe(false); // Attack right by 3
+});
